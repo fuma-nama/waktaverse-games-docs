f298c93779582e3a24c1b21ff71ff2f5b4ae7ddf
DO_S <rlaxogus7094@naver.com>
2025-03-15 12:32:21 +0900

    style: 폰트 및 로고 변경

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
 
 /**
  * Shared layout configurations
@@ -11,14 +12,9 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
+        <div className="size-6">
+          <Image src="/images/logo.svg" alt="Logo" width={24} height={24} />
+        </div>
         Waktaverse Games Docs
       </>
     ),
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import type { ReactNode } from "react";
+import localFont from "next/font/local";
 
-const inter = Inter({
-  subsets: ['latin'],
+const wanted = localFont({
+  src: "../public/font/WantedSansVariable.woff2",
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="ko" className={wanted.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
