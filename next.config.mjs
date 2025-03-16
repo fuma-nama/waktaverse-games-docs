d24bab7a306b870e88b8994b9ef7750b893d738d
DO_S <rlaxogus7094@naver.com>
2025-03-16 13:21:50 +0900

    fix: 원상 복구
    Discussion 통해서 공식 답변 대기 중

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,6 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api-reference/:path*",
-        destination: "/api/fallback-docs/:path*",
-      },
-    ];
-  },
 };
 
 export default withMDX(config);
